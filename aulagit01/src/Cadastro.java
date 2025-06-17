db28401ac389cb8d72fbe59873818b2ce61afbd8
Laysiene Silva <laysiene.silva@outlook.com>
2025-06-17 10:58:59 -0300

    Incluindo o campo Nome na classe Castrado

--- aulagit01/src/Cadastro.java
+++ aulagit01/src/Cadastro.java
@@ -1,5 +1,6 @@
 public class Cadastro {
     public static void main(String[] args) {
+        System.out.println("Nome: ");
         System.out.println("Login: ");
         System.out.println("Senha: ");
     }
